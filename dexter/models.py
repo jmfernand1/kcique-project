2b8933a473a7b418d67bf8b367e53ae8c9eca431
Juan Manuel Fernandez Ceballos <jmfernand1@eafit.edu.co>
2025-12-27 10:24:57 -0500

    se agrega columna tipo json para los id de sharepoint

--- dexter/models.py
+++ dexter/models.py
@@ -416,6 +416,7 @@ class Desembolso(models.Model):
     estado = models.CharField(max_length=50, null=True, blank=True)
     tipo_desembolso = models.ForeignKey(TipoDesembolso, on_delete=models.CASCADE, related_name='desembolsos', null=True, blank=True)
     observaciones = models.JSONField(null=True, blank=True, default=dict)
+    ids_sharepoint = models.JSONField(null=True, blank=True, default=dict)
 
     def __str__(self):
         return f"Desembolso {self.referencia}"
